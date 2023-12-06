dae114f80d3ee54987ab18425462fce5c9dcdc00
Simon Harvey <simon.k.harv@gmail.com>
2023-12-06 21:59:32 +1100

    finding numbers

--- src/day3.rs
+++ src/day3.rs
@@ -1,84 +1,77 @@
 use super::utils;
 
-struct Location {
-    x: i32,
-    y: i32,
-    gifts: u32,
+struct Number {
+    start_x: usize,
+    end_x: usize,
+    y_pos: usize,
+    value: u32,
+    is_part: bool,
+}
+
+struct MappedChar {
+    x_pos: usize,
+    y_pos: usize,
+    character: char,
 }
 
 pub fn run() {
     if let Ok(lines) = utils::read_lines("./input/day3") {
-        let mut locations = Vec::<Location>::new();
-        let mut santa_pos: (i32, i32) = (0, 0);
-        let mut robo_santa_pos: (i32, i32) = (0, 0);
-        let origin = Location{x: santa_pos.0, y: santa_pos.1, gifts: 1};
-        locations.push(origin);
-        let mut total = 0;
-
-        for line in lines {
+        let mut numbers = Vec::<Number>::new();
+        let mut mapped_chars = Vec::<MappedChar>::new();
+        for (y, line) in lines.enumerate() {
             if let Ok(text) = line {
-                for (index, char) in text.chars().enumerate() {
-                    let location_change = translate_direction(char);
-                    if index % 2 != 0 {
-                        santa_pos.0 += location_change.0;
-                        santa_pos.1 += location_change.1;
-
-                        let mut location_exists = false;
-                        for location in locations.iter_mut() {
-                            if location.x == santa_pos.0 && location.y == santa_pos.1 {
-                                location_exists = true;
-                                location.gifts += 1;
-                                println!("{},{} now has {} gifts, thanks Santa!",  location.x, location.y, location.gifts);
-                            }
-                        }
-                        if !location_exists {
-                            locations.push(Location{x: santa_pos.0, y: santa_pos.1, gifts: 1});
-                            if let Some(last) = locations.last() {
-                                println!("{},{} now has {} gifts, thanks Santa!",  last.x, last.y, last.gifts);
-                            }
-                        }
-                    } else {
-                        robo_santa_pos.0 += location_change.0;
-                        robo_santa_pos.1 += location_change.1;
-
-                        let mut location_exists = false;
-                        for location in locations.iter_mut() {
-                            if location.x == robo_santa_pos.0 && location.y == robo_santa_pos.1 {
-                                location_exists = true;
-                                location.gifts += 1;
-                                println!("{},{} now has {} gifts, thanks Robo Santa!",  location.x, location.y, location.gifts);
-                            }
-                        }
-                        if !location_exists {
-                            locations.push(Location{x: robo_santa_pos.0, y: robo_santa_pos.1, gifts: 1});
-                            if let Some(last) = locations.last() {
-                                println!("{},{} now has {} gifts, thanks Robo Santa!",  last.x, last.y, last.gifts);
-                            }
-                        }
-
+                let mut skip_until_not_digit = false;
+                for (x, char) in text.chars().enumerate() {
+                    if !char.is_digit(10) {
+                        skip_until_not_digit = false;
                     }
-                }
-                for location in locations.iter() {
-                    if location.gifts >= 1 {
-                        total += 1;
+
+                    if skip_until_not_digit {
+                        continue;
+                    }
+
+                    if !char_is_period(&char) {
+                        mapped_chars.push(MappedChar{x_pos: x, y_pos: y, character: char});
+                    }
+
+                    if char.is_digit(10) {
+                        skip_until_not_digit = true;
+                        if let Some(found_number) = get_number(&text, x){
+                            let number = Number {
+                                start_x: found_number.1,
+                                end_x: found_number.2,
+                                y_pos: y,
+                                value: found_number.0,
+                                is_part: false
+                            };
+                            println!("{}", number.value);
+                        }
                     }
                 }
             }
         }
-
-        println!("Part 1 Answer: {}", total);
     }
 }
 
-fn translate_direction(direction: char) -> (i32, i32) {
-    match direction {
-        '^' => return (0, 1),
-        '>' => return (1, 0),
-        '<' => return (-1, 0),
-        'v' => return (0, -1),
-        _ => {
-            println!("ERROR");
-            return (0, 0);
+fn get_number(text: &String, from: usize) -> Option<(u32, usize, usize)> {
+    if let Some(clipped_text) = text.get(from..) {
+        for (i, char) in clipped_text.chars().enumerate() {
+            if !char.is_digit(10) {
+                let num_string = clipped_text.get(0..i);
+                if let Some(number) = num_string {
+                    if let Ok(parsed_num) = number.parse::<u32>() {
+                        return Some((parsed_num, from, from + i))
+                    }
+                }
+            }
         }
+    }
+    return None;
+}
+
+fn char_is_period(character: &char) -> bool {
+    match character {
+        '.' => true,
+        _ => false
     }
 }
